5aa9b55a092a21f8e7d497e9199e7911199ff12c
ninimefarishvili <164371339+ninimefarishvili@users.noreply.github.com>
2025-06-05 19:19:01 +0400

    Update Schema.py

--- tasks/Schema.py
+++ tasks/Schema.py
@@ -1,23 +1,31 @@
-from datetime import date
+from datetime import date, datetime
 from typing import Optional
 from django_ninja import Schema
 
 class TaskBaseSchema(Schema):
     title: str
-    description:str = ""
+    description: str = ""
     deadline: date
-    status :str
+    status: str
 
-class TaskCreateScema(TaskBaseSchema):
-    pass
+class TaskCreateSchema(TaskBaseSchema):
+    client_id: str  # required to simulate user
 
 class TaskUpdateSchema(Schema):
-    title : Optional[str] = None
+    title: Optional[str] = None
     description: Optional[str] = None
-    deadline: Optional[str] = None
+    deadline: Optional[date] = None
     status: Optional[str] = None
 
-class taskSchems(TaskBaseSchema):
-    id : int
-    created_at :date
-    overdue : bool
+class TaskSchema(TaskBaseSchema):
+    id: int
+    created_at: datetime
+    overdue: bool
+
+    @classmethod
+    def from_orm(cls, task):
+        # Override to calculate overdue dynamically
+        obj = super().from_orm(task)
+        obj.overdue = task.deadline < date.today() and task.status == task.STATUS_PENDING
+        return obj
+
